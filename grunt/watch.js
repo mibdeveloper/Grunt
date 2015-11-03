b0a88296c6449c8b43d54b1083bef36b6ef204e6
Viacheslav Radchenko <mibdeveloper@gmail.com>
2015-11-03 16:34:27 +0200

    watch configured

---
+++ grunt/watch.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+        options: {
+            spawn: false,
+            livereload: true
+        },
+
+        scripts: {
+            files: [
+                'src/scripts/*.js'
+            ],
+            tasks: [
+                /*'jshint',
+                'uglify'*/
+            ]
+        },
+
+        styles: {
+            files: [
+                'src/styles/*.scss',
+                'src/styles/*.css'
+            ],
+            tasks: [
+                /*'sass:dev'*/
+            ]
+        },
+
+        html: {
+            files: [
+                'src/**/*.html'
+            ],
+            tasks: [
+                /*'sass:dev'*/
+            ]
+        },
+
+
+
+
+
+
+};
